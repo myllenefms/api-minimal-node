a5648fa0da0e13a311de2e24414b8121771e5c08
Myllene Mendes <mfrsilva@mapfre.com.br>
2025-06-04 14:28:42 -0300

    bugfix+feat/correção dos IDs dos personagens e criação de rota para filtrar personagem por filme

--- src/server.ts
+++ src/server.ts
@@ -16,14 +16,14 @@ const movies = [
 
     const characters = [
         {id:1, name: "Luke Skywalker", movie: [4,5,6,8]}, 
-        {id:1, name: "Leia Skywalker", movie: [4,5,6]}, 
-        {id:1, name: "Anakin Skywalker", movie: [1,2,3]}, 
-        {id:1, name: "Darth Vader", movie: [4,5,6]}, 
-        {id:1, name: "Obi-Wan Kenobi", movie: [1,2,3,4,5,6]}, 
-        {id:1, name: "Yoda", movie: [1,2,3,5,6,8]}, 
-        {id:1, name: "Rey", movie: [7,8,9]}, 
-        {id:1, name: "Padmé Amidala", movie: [1,2,3]}, 
-        {id:1, name: "Han Solo", movie: [4,5,6,7,9]}, 
+        {id:2, name: "Leia Skywalker", movie: [4,5,6]}, 
+        {id:3, name: "Anakin Skywalker", movie: [1,2,3]}, 
+        {id:4, name: "Darth Vader", movie: [4,5,6]}, 
+        {id:5, name: "Obi-Wan Kenobi", movie: [1,2,3,4,5,6]}, 
+        {id:6, name: "Yoda", movie: [1,2,3,5,6,8]}, 
+        {id:7, name: "Rey", movie: [7,8,9]}, 
+        {id:8, name: "Padmé Amidala", movie: [1,2,3]}, 
+        {id:9, name: "Han Solo", movie: [4,5,6,7,9]}, 
     ]
 
 server.get("/movies",async(request, response) => {
@@ -35,6 +35,31 @@ server.get("/characters", async (request, response) => {
     response.type("application/json").code(200)
     return characters
 });
+
+interface CharacterParams{
+    movie: string
+}
+
+server.get<{Params: CharacterParams}>("/characters/:movie", async (request, response) => {
+    const param = parseInt(request.params.movie);
+    const filterCharac = characters.filter( c => c.movie.includes(param));
+    const filterMovie = movies.find( m => m.id === param);
+
+    if (!filterMovie){
+        response.type("application/json").code(404)
+        return { message: "Movie Not Found"}
+    }
+
+    if (filterCharac.length === 0){
+        response.type("application/json").code(204)
+        return { message: "None character in that Movie"}
+    }
+
+    response.type("application/json").code(200)
+    return { filterCharac }
+
+})
+
 
 server.listen({port: 3333}, () => {
     console.log("Server init");
